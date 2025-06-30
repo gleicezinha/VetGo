d4b1a12a487582008ac3bb5730ad5ee6d29b413b
gleicezinha <gleicebeatriz11@gmail.com>
2025-06-30 12:37:20 -0300

    fix: back e front

---
+++ vetgoapi/src/main/java/com/vetgo/api/App.java
@@ -0,0 +1,13 @@
+package com.vetgo.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
